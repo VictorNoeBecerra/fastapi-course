613def80fff51b06615f3f6f4c230809faf62cac
Victor Noé  Becerra Hernández <victor.becerra@aiscs.com.mx>
2024-12-04 14:54:19 -0600

    Update models.py
    From models.by, the model for manage relationships between customers and plans much to much has been named CustomerPlan and it has configured

--- curso-fastapi-project/models.py
+++ curso-fastapi-project/models.py
@@ -1,49 +1,75 @@
-#models.py
+# models.py
 
-from typing import List, Optional
 from pydantic import BaseModel
 from sqlmodel import Field, Relationship, SQLModel
+from enum import Enum
+
+class StatusEnum(str, Enum):
+    ACTIVE = "active"
+    INACTIVE = "inactive"
+    
+class CustomerPlan(SQLModel, table=True):
+    id: int = Field(primary_key=True)
+    plan_id: int = Field(foreign_key="plan.id")
+    customer_id: int = Field(foreign_key="customer.id")
+    status: StatusEnum = Field(default=StatusEnum.ACTIVE)
+
+class Plan(SQLModel, table=True):
+    id: int | None = Field(primary_key=True)
+    name: str = Field(default=None)
+    price: int = Field(default=None)
+    descripcion: str = Field(default=None)
+    customers: list["Customer"] = Relationship(
+        back_populates="plans", link_model=CustomerPlan
+    )
+
 
 class CustomerBase(SQLModel):
     name: str = Field(default=None)
-    description: Optional[str] = Field(default=None)
+    description: str | None = Field(default=None)
     email: str = Field(default=None)
-    age:int = Field(default=None)
-    
+    age: int = Field(default=None)
+
+
 class CustomerCreate(CustomerBase):
     pass
 
+
 class CustomerUpdate(CustomerBase):
     pass
-    
+
+
 class Customer(CustomerBase, table=True):
-    id: Optional[int] = Field(default=None, primary_key=True)
-    
-class TransactionBase(SQLModel):#transacciones
-    amount: int
-    description: Optional[str]
+    id: int | None = Field(default=None, primary_key=True)
+    transactions: list["Transaction"] = Relationship(back_populates="customer")
+    plans: list[Plan] = Relationship(
+        back_populates="customers", link_model=CustomerPlan
+    )
+
+
+class TransactionBase(SQLModel):
+    ammount: int
+    description: str
+
+
+class Transaction(TransactionBase, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    customer_id: int = Field(foreign_key="customer.id")
+    customer: Customer = Relationship(back_populates="transactions")
+
 
 class TransactionCreate(TransactionBase):
+    customer_id: int = Field(foreign_key="customer.id")
+
+class TransactionUpdate(TransactionCreate):
     pass
 
-class TransactionUpdate(TransactionBase):
-    pass
-
-class Transaction(TransactionBase, table=True):#transacciones
-    id: int = Field(default=None, primary_key=True)
-    # customer_id: int = Field(default=None, foreign_key="customer.id")
-    
-class InvoiceBase(SQLModel):
+class Invoice(BaseModel):
+    id: int
+    customer: Customer
+    transactions: list[Transaction]
     total: int
 
-class InvoiceCreate(InvoiceBase):
-    customer_id: int
-    transactions: List[Transaction]
-
-class InvoiceUpdate(InvoiceBase):
-    pass
-
-class Invoice(InvoiceBase, table=True):
-    id: Optional[int] = Field(default=None, primary_key=True)
-    customer_id: int = Field(foreign_key="customer.id")
-    customer: Optional[Customer] = Relationship(back_populates="invoices")
+    @property
+    def ammount_total(self):
+        return sum(transaction.ammount for transaction in self.transactions)
